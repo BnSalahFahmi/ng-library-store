fb933a680f771c5642f69544e401d8c76948288d
BnSalahFahmi <fahmii.bensalah@gmail.com>
2019-12-26 14:29:49 +0100

    redirect to not found component if path not found

--- ng-library-store-client/src/app/app-routing.module.ts
+++ ng-library-store-client/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'library', loadChildren: () => import('./library/library.module').then(m => m.LibraryModule) },
   { path: 'book', loadChildren: () => import('./book/book.module').then(m => m.BookModule) },
   { path: '', redirectTo: 'library', pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
